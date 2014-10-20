6d8801ecedbe3a5c819b566c09b4cf2195b2c50b
Zhipeng Zhang <qq1126918258@gmail.com>
2014-10-20 21:46:40 +0800

    init

---
+++ LeShou/Leshou/MainForm.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace LeShou
+{
+    partial class login_form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webser = new System.Windows.Forms.WebBrowser();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.tbx_code = new System.Windows.Forms.TextBox();
+            this.pic_code = new System.Windows.Forms.PictureBox();
+            this.tbx_userid = new System.Windows.Forms.TextBox();
+            this.tbx_pwd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.timer_load = new System.Windows.Forms.Timer(this.components);
+            this.panel_login = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.登录ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.发布ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.删除ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_end = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.num_up_dowm_delete = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_code)).BeginInit();
+            this.panel_login.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_up_dowm_delete)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webser
+            // 
+            this.webser.Location = new System.Drawing.Point(687, 118);
+            this.webser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webser.Name = "webser";
+            this.webser.Size = new System.Drawing.Size(286, 255);
+            this.webser.TabIndex = 0;
+            this.webser.TabStop = false;
+            this.webser.WebBrowserShortcutsEnabled = false;
+            this.webser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webser_Navigated);
+            this.webser.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webser_Navigating);
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(32, 127);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(44, 23);
+            this.btn_open.TabIndex = 2;
+            this.btn_open.Text = "刷新";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // tbx_code
+            // 
+            this.tbx_code.Location = new System.Drawing.Point(88, 90);
+            this.tbx_code.Name = "tbx_code";
+            this.tbx_code.Size = new System.Drawing.Size(75, 21);
+            this.tbx_code.TabIndex = 2;
+            // 
+            // pic_code
+            // 
+            this.pic_code.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pic_code.Location = new System.Drawing.Point(169, 86);
+            this.pic_code.Name = "pic_code";
+            this.pic_code.Size = new System.Drawing.Size(95, 31);
+            this.pic_code.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_code.TabIndex = 6;
+            this.pic_code.TabStop = false;
+            this.pic_code.Click += new System.EventHandler(this.pic_code_Click);
+            // 
+            // tbx_userid
+            // 
+            this.tbx_userid.Location = new System.Drawing.Point(88, 34);
+            this.tbx_userid.Name = "tbx_userid";
+            this.tbx_userid.Size = new System.Drawing.Size(176, 21);
+            this.tbx_userid.TabIndex = 0;
+            this.tbx_userid.Text = "ycydwlyxgs012";
+            // 
+            // tbx_pwd
+            // 
+            this.tbx_pwd.Location = new System.Drawing.Point(88, 61);
+            this.tbx_pwd.Name = "tbx_pwd";
+            this.tbx_pwd.PasswordChar = '*';
+            this.tbx_pwd.Size = new System.Drawing.Size(176, 21);
+            this.tbx_pwd.TabIndex = 1;
+            this.tbx_pwd.Text = "102596";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "帐号";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "密码";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "验证码";
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(99, 127);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(45, 23);
+            this.btn_login.TabIndex = 3;
+            this.btn_login.Text = "登录";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // timer_load
+            // 
+            this.timer_load.Interval = 1000;
+            this.timer_load.Tick += new System.EventHandler(this.timer_load_Tick);
+            // 
+            // panel_login
+            // 
+            this.panel_login.Controls.Add(this.label1);
+            this.panel_login.Controls.Add(this.btn_login);
+            this.panel_login.Controls.Add(this.btn_open);
+            this.panel_login.Controls.Add(this.label3);
+            this.panel_login.Controls.Add(this.tbx_code);
+            this.panel_login.Controls.Add(this.label2);
+            this.panel_login.Controls.Add(this.pic_code);
+            this.panel_login.Controls.Add(this.tbx_userid);
+            this.panel_login.Controls.Add(this.tbx_pwd);
+            this.panel_login.Location = new System.Drawing.Point(12, 26);
+            this.panel_login.Name = "panel_login";
+            this.panel_login.Size = new System.Drawing.Size(296, 200);
+            this.panel_login.TabIndex = 12;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.登录ToolStripMenuItem,
+            this.发布ToolStripMenuItem,
+            this.删除ToolStripMenuItem,
+            this.删除ToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1044, 25);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 登录ToolStripMenuItem
+            // 
+            this.登录ToolStripMenuItem.Name = "登录ToolStripMenuItem";
+            this.登录ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.登录ToolStripMenuItem.Text = "登录";
+            // 
+            // 发布ToolStripMenuItem
+            // 
+            this.发布ToolStripMenuItem.Name = "发布ToolStripMenuItem";
+            this.发布ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.发布ToolStripMenuItem.Text = "发布";
+            // 
+            // 删除ToolStripMenuItem
+            // 
+            this.删除ToolStripMenuItem.Name = "删除ToolStripMenuItem";
+            this.删除ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.删除ToolStripMenuItem.Text = "删除";
+            // 
+            // 删除ToolStripMenuItem1
+            // 
+            this.删除ToolStripMenuItem1.Name = "删除ToolStripMenuItem1";
+            this.删除ToolStripMenuItem1.Size = new System.Drawing.Size(44, 21);
+            this.删除ToolStripMenuItem1.Text = "退出";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.num_up_dowm_delete);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btn_end);
+            this.panel1.Controls.Add(this.btn_delete);
+            this.panel1.Location = new System.Drawing.Point(328, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(296, 200);
+            this.panel1.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "条数";
+            // 
+            // btn_end
+            // 
+            this.btn_end.Location = new System.Drawing.Point(141, 107);
+            this.btn_end.Name = "btn_end";
+            this.btn_end.Size = new System.Drawing.Size(61, 33);
+            this.btn_end.TabIndex = 3;
+            this.btn_end.Text = "结束";
+            this.btn_end.UseVisualStyleBackColor = true;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(57, 107);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(61, 33);
+            this.btn_delete.TabIndex = 2;
+            this.btn_delete.Text = "删除";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // num_up_dowm_delete
+            // 
+            this.num_up_dowm_delete.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.num_up_dowm_delete.InterceptArrowKeys = false;
+            this.num_up_dowm_delete.Location = new System.Drawing.Point(106, 47);
+            this.num_up_dowm_delete.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.num_up_dowm_delete.Name = "num_up_dowm_delete";
+            this.num_up_dowm_delete.Size = new System.Drawing.Size(125, 21);
+            this.num_up_dowm_delete.TabIndex = 10;
+            this.num_up_dowm_delete.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.num_up_dowm_delete.ThousandsSeparator = true;
+            this.num_up_dowm_delete.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // login_form
+            // 
+            this.AcceptButton = this.btn_login;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1044, 461);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel_login);
+            this.Controls.Add(this.webser);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "login_form";
+            this.Text = "乐收";
+            this.Load += new System.EventHandler(this.login_form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_code)).EndInit();
+            this.panel_login.ResumeLayout(false);
+            this.panel_login.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_up_dowm_delete)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webser;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.TextBox tbx_code;
+        private System.Windows.Forms.PictureBox pic_code;
+        private System.Windows.Forms.TextBox tbx_userid;
+        private System.Windows.Forms.TextBox tbx_pwd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Timer timer_load;
+        private System.Windows.Forms.Panel panel_login;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 登录ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 发布ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 删除ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 删除ToolStripMenuItem1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_end;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.NumericUpDown num_up_dowm_delete;
+    }
+}
+
---
+++ LeShou/Leshou/MainForm.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Net;
+
+namespace LeShou
+{
+
+    public partial class login_form : Form
+    {
+        private HtmlDocument loginform = null;
+        /// <summary>
+        /// 验证码地址
+        /// </summary>
+        private static string code_url = "http://leshou.com/user/validate.png";
+        /// <summary>
+        /// 登录地址
+        /// </summary>
+        private static string login_url = "http://leshou.com/login";
+        /// <summary>
+        /// 删除页地址
+        /// </summary>
+        private static string delete_page_url = "http://leshou.com/my/?comp=news&page=";
+        /// <summary>
+        /// 最大删除页面
+        /// </summary>
+        private static int max_delete_page_num = 999999999;
+        /// <summary>
+        /// 删除地址
+        /// </summary>
+        private static string delete_url = "http://leshou.com/my/?comp=news&task=deletenews&nid=";
+        /// <summary>
+        /// 每页条数
+        /// </summary>
+        private static int page_size = 30;
+        /// <summary>
+        /// 验证码图片路径
+        /// </summary>
+        private static string code_path = ".code";
+
+        public login_form()
+        {
+            InitializeComponent();
+        }
+
+        void Window_Error(object sender, HtmlElementErrorEventArgs e)
+        {
+            e.Handled = true;
+        }
+        /// <summary> 载入登录页面初始化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            web_loaction(login_url);
+        }
+        private void login_form_Load(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        /// <summary> 登录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            write_html();
+            login();
+        }
+        /// <summary> 浏览器导航
+        /// </summary>
+        public void web_loaction(string url)
+        {
+            this.webser.Navigate(url);
+        }
+
+        /// <summary>初始化登录页面，并且绘制验证码
+        /// </summary>
+        public void init()
+        {
+            web_loaction(login_url);
+        }
+
+
+        /// <summary> 填写html
+        /// </summary>
+        public void write_html()
+        {
+            string USERID = this.tbx_userid.Text;
+            string PWD = this.tbx_pwd.Text;
+            string CODE = this.tbx_code.Text;
+            loginform.GetElementById("username").SetAttribute("value", USERID);
+            loginform.GetElementById("password").SetAttribute("value", PWD);
+            loginform.GetElementById("code").SetAttribute("value", CODE);
+        }
+
+        /// <summary>检查帐号 密码 验证码</summary>
+        /// <returns>True/False</returns>
+        public bool check()
+        {
+            bool state = true;
+            string USERID = this.tbx_userid.Text;
+            string PWD = this.tbx_pwd.Text;
+            string CODE = this.tbx_code.Text;
+            if (USERID.Length < 16)
+            {
+                MessageBox.Show("请重新输入身份证!");
+                state = false;
+            }
+            if (PWD.Length < 6)
+            {
+                MessageBox.Show("请重新输入密码!");
+                state = false;
+            }
+            if (CODE.Length != 5)
+            {
+                MessageBox.Show("请重新输入验证码!");
+                state = false;
+            }
+            return state;
+        }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        public void login()
+        {
+            HtmlElement check_remeber = loginform.GetElementsByTagName("input")[3];
+            check_remeber.InvokeMember("Click");
+            HtmlElement btnAdd = loginform.GetElementsByTagName("input")[4];
+            btnAdd.InvokeMember("Click");
+        }
+        private void webser_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            //timer_load.Enabled = true;
+        }
+
+        private void timer_load_Tick(object sender, EventArgs e)
+        {
+            if (webser.ReadyState == WebBrowserReadyState.Complete && webser.IsBusy == false)
+            {
+                timer_load.Enabled = false;
+                if (webser.StatusText == "完成" && webser.Url.AbsoluteUri == login_url)
+                {
+                    load_img();
+                    tbx_userid.Focus();
+                    tbx_userid.SelectAll();
+                }
+            }
+        }
+        /// <summary>
+        /// 加载验证码
+        /// </summary>
+        private void load_img()
+        {
+            loginform = webser.Document.Forms[0].Document;
+            save_image_by_list_byte();
+            this.pic_code.ImageLocation = code_path;
+        }
+
+        private void webser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            timer_load.Enabled = true;
+        }
+
+        private void pic_code_Click(object sender, EventArgs e)
+        {
+            load_img();
+        }
+
+        /// <summary>
+        /// 将字节列表 保存为图片
+        /// </summary>
+        public void save_image_by_list_byte()
+        {
+            byte[] byte_list = RequestByCookie(code_url, webser.Document.Cookie);
+            File.WriteAllBytes(code_path, byte_list.ToArray());
+        }
+        /// <summary>
+        /// request 请求
+        /// </summary>
+        /// <param name="url">URL</param>
+        /// <param name="cookie">Cookies值</param>
+        /// <param name="is_get">request maehod.is get? or false:post.</param>
+        public static byte[] RequestByCookie(string url, string cookie, bool is_get = true)
+        {
+            List<byte> list = new List<byte>();
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+            //属性配置
+            webRequest.AllowWriteStreamBuffering = true;
+            webRequest.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            webRequest.MaximumResponseHeadersLength = -1;
+            webRequest.Headers.Add("cookie", cookie);
+            if (is_get == true) webRequest.Method = "GET";
+            else webRequest.Method = "POST";
+            webRequest.KeepAlive = true;
+            try
+            {
+                //获取服务器返回的资源
+                using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse())
+                {
+                    using (Stream stream = webResponse.GetResponseStream())
+                    {
+
+                        while (true)
+                        {
+                            int data = stream.ReadByte();
+                            if (data == -1)
+                                break;
+                            list.Add((byte)data);
+                        }
+                        //return BytesToImage(list.ToArray());
+                        //return System.Drawing.Image.FromFile(".code", true);
+                        //image = System.Drawing.Image.FromStream(stream, true, true);
+
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return list.ToArray();
+        }
+        /// <summary>
+        /// Convert Byte[] to Image
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static Image BytesToImage(byte[] buffer)
+        {
+            MemoryStream ms = new MemoryStream(buffer);
+            Image image = System.Drawing.Image.FromStream(ms, true);
+            ms.Close();
+            return image;
+        }
+        /// <summary>
+        /// Convert Byte[] to string
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static string BytesToString(byte[] buffer)
+        {
+            string result = Encoding.UTF8.GetString(buffer);
+            return result;
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            get_delete_page(max_delete_page_num);
+        }
+
+        private void get_delete_page(int page_num)
+        {
+            List<int> l_int = new List<int>();
+            int max_page = 1;
+            int delete_num = (int)this.num_up_dowm_delete.Value;
+            string url = delete_page_url + page_num;
+            byte[] byte_result = RequestByCookie(url, webser.Document.Cookie);
+            string str_html = BytesToString(byte_result);
+            MatchCollection match = Regex.Matches(str_html, @"page=(\d+)", RegexOptions.Singleline);
+            if (match.Count > 0) max_page = Convert.ToInt32(match[match.Count - 1].Groups[1].Value);
+
+            l_int.AddRange(re_page_data_count(str_html));
+
+        }
+
+        /// <summary>
+        /// 正则 -- 匹配每页的帖子id
+        /// </summary>
+        /// <param name="html">html string</param>
+        /// <returns>list int</returns>
+        private List<int> re_page_data_count(string html)
+        {
+            List<int> l_int = new List<int>();
+            MatchCollection match_nid = Regex.Matches(html, @"deletenews&nid=(\d+)", RegexOptions.Singleline);
+            for (int i = 0; i < match_nid.Count; i++)
+            {
+                l_int.Add(Convert.ToInt32(match_nid[i].Groups[1].Value));
+            }
+            return l_int;
+        }
+
+    }
+
+
+
+
+}
